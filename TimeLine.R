44f82ba0c3e1d1e52d14a5633ce4562a8c58fb71
Arto Moilanen <arto@gnz.fi>
2021-04-18 18:46:16 +0300

    excercise week 4

--- TimeLine.R
+++ TimeLine.R
@@ -1,4 +1,5 @@
-my_file <- "my_Scopus_TSE_articles_clean_data.RData"
+my_file <- "my_Scopus_www_articles_clean_data.RData"
+my_file <- "my_STO_www_data_clean.RData"
 
 #draw_myWordCloud = function(my_file){
 
@@ -14,6 +15,7 @@ load(my_temp_file)
 
 #Take years from date 
 #(Hint lot of online help for various conversions availabe )
+#install.packages("lubridate")
 years <- lubridate::year(my_articles$Date)
 class(years)
 class(my_articles)
@@ -23,7 +25,7 @@ class(my_articles$Date)
 yearly <- table (years)
 class(yearly)
 plot (yearly, type ="l", xaxt="n", xlab="", ylab="")
-axis(1, at = seq(1975, 2018, by = 1), las=2)
+axis(1, at = seq(1975, 2021, by = 1), las=2)
 
 #use pure date
 date <- table (my_articles$Date)
@@ -52,6 +54,7 @@ boxplot(l100km~cyl,data=mtcars, main="Car Milage Data",
         xlab="Number of Cylinders", ylab="liters per 100km") 
 
 
+#install.packages(vioplot)
 library(vioplot)
 #Same info as in box plot but more illustrative
 vioplot(my_articles$Cites)
@@ -81,6 +84,7 @@ median(my_articles2$Cites[my_articles2$Date <= median(my_articles2$Date)])
 #Notice the minus sign descending order
 head(my_articles$Cites[order(-my_articles$Cites)], n=5)
 head(my_articles$Title[order(-my_articles$Cites)], n=5)
+head(my_articles$Abstract[order(-my_articles$Cites)], n=5)
 
 #This is optional just for kick analysis. Feel free to added to your report if you like. 
 #We can test more more things. Recently it was claimed that in econometrics that shorter articles get more citations. Lets see
@@ -89,7 +93,7 @@ head(my_articles$Title[order(-my_articles$Cites)], n=5)
 median (nchar(my_articles2$Title))
 
 boxplot(my_articles2$Cites[nchar(my_articles2$Title) > median (nchar(my_articles2$Title))],
-        my_articles2$Cites[nchar(my_articles2$Title) <= median (nchar(my_articles2$Title))], names=c("longer", "shorter"), main="IEEE Transaction on Software Engineering")
+        my_articles2$Cites[nchar(my_articles2$Title) <= median (nchar(my_articles2$Title))], names=c("longer", "shorter"), main="World Wide Web")
 
 wilcox.test(my_articles2$Cites[nchar(my_articles2$Title) > median (nchar(my_articles2$Title))],
        my_articles2$Cites[nchar(my_articles2$Title) <= median (nchar(my_articles2$Title))])
@@ -105,12 +109,17 @@ q3_t <- my_articles$Cites[nchar(my_articles2$Title) > quantile(nchar(my_articles
                             nchar(my_articles2$Title) <= quantile(nchar(my_articles2$Title), probs = 0.75)]
 q4_t <- my_articles$Cites[nchar(my_articles2$Title) > quantile(nchar(my_articles2$Title), probs = 0.75)]
 
-boxplot(q1_t,q2_t, q3_t, q4_t, names=c("shorter", "short", "long", "longer"), main="Title Length(x) & Citations(y)\nIEEE Transaction on Software Engineering")
+if(q1_t < 0)
+{
+        q1_t=0
+}
+
+boxplot(q1_t,q2_t, q3_t, q4_t, names=c("shorter", "short", "long", "longer"), main="Title Length(x) & Citations(y)\nWorld Wide Web")
 summary(q1_t)
 summary(q2_t)
 summary(q3_t)
 summary(q4_t)
 
 #Lets try log scale. We need to add +1 since log(0) is undefined and log(1) is zero
-boxplot(q1_t+1,q2_t+1, q3_t+1, q4_t+1,log="y", names=c("shorter", "short", "long", "longer"), main="Title length(x) & Citations(y)\nIEEE Transaction on Software Engineering\n")
+boxplot(q1_t+1,q2_t+1, q3_t+1, q4_t+1,log="y", names=c("shorter", "short", "long", "longer"), main="Title length(x) & Citations(y)\nWorld Wide Web\n")
 
